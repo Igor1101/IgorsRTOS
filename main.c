4523c1a900137d2cb9e589bf65d78b2686a5acb5
Igor1101 <Igor1101@github.com>
2019-07-26 15:21:07 +0300

    Added better example

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <rtos.h>
+void task_f(void*msg);
+rtostask_t task1 = {
+		.msg = "TASK1",
+		.function = task_f
+};
 int main()
 {
 	rtos_init();
+	rtos_sleep(TIMER_S(1));
+	rtos_add_task(&task1);
 	pr_info("starting igorsrtos" " " RTOS_PORT_NAME);
 	while(1);
+}
+
+void task_f(void*msg)
+{
+	if(msg != NULL)
+		pr_info("%s",(char*)msg);
 }
